cd1713179197e9c07a5b282fb5ed0a9c858ae567
maaron95 <maaron@atl.yearup.org>
2015-09-23 07:50:04 -0400

    Git file project 1

---
+++ Project1/src/Intro.java
@@ -0,0 +1,12 @@
+
+public class Intro {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("My name is Mark.");
+		System.out.println("My favorite movie is The Wood.");
+		System.out.println("I play basketball every chance I get.");
+	}
+}
